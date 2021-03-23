dbf8da323da4a0aa31bab9e1b5fb602b04bbe14f
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-23 18:25:41 +0530

    logout and private rotes setup

--- src/components/auth/PrivateRoute.js
+++ src/components/auth/PrivateRoute.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Route, Redirect } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { getToken } from '../../Redux'
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
+    const accessToken = useSelector(state => state.accessToken)
+    console.log(accessToken);
     return (
         <Route
             {...rest}
             render={props =>
-                <Component {...props} />
+                accessToken && accessToken.payload.length > 0 ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: { from: props.location }
+                        }}
+                    />
+                )
             }
         />
     )
