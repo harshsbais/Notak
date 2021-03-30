b0377bbfa72d33da382f29aa0601c220af6300e9
Harsh Singh Bais <harshsbais5@gmail.com>
2021-03-30 20:51:44 +0530

    change styling on the home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Home.css'
+import logo from '../../logo.png';
 function Home() {
     return (
-        <div>
-            <center><h1>Notak</h1>
+        <div className="home">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#" ><img style={{ height: "8vh" }} src={logo} /></a>
+            </nav>
+            <center><h1 style={{ fontFamily: "Dancing Script", fontSize: "100px" }}>Notak</h1>
                 <br />
-                <h3>Let's revolutionise note taking</h3>
-                <Link to="/login">Login</Link>
-                <br />
-                <Link to="/signup">Signup</Link>
+                <h3 style={{ fontFamily: "Dancing Script" }}>Let's revolutionise note taking</h3>
+                <div style={{ marginTop: "50px" }}>
+                    <Link to="/login"><Button style={{ color: "black", backgroundImage: "linear-gradient(to right, #84fab0 0%, #8fd3f4 51%, #84fab0 100%)", borderRadius: "10px", height: "50px", marginRight: "5vw", width: "150px" }}>Login</Button></Link>
+                    <Link to="/signup"><Button style={{ color: "black", backgroundImage: "linear-gradient(to right, #84fab0 0%, #8fd3f4 51%, #84fab0 100%)", borderRadius: "10px", height: "50px", width: "150px" }}>SignUp</Button></Link>
+                </div>
             </center>
-        </div>
+        </div >
     )
 }
 
